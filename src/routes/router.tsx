fdd0441bb8b8e20589aa0afe8c63ee935f27d503
Joshua Arciniegas R. <162453112+Darkmooncito@users.noreply.github.com>
2025-12-16 17:22:54 -0500

    feat: Configure routes and update dependencies for complete application

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,16 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/home/Home";
+import { createBrowserRouter } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { RoomPage } from '../pages/RoomPage';
 
 export const routes = [
-    {
-        path: "/",
-        element: <Home />,
-    },
-]
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/room/:roomId',
+    element: <RoomPage />,
+  },
+];
 
 export const router = createBrowserRouter(routes);
